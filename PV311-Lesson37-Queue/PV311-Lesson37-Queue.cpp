6d84b4e6a4eba306c070bd15c8a9a5035ea0c959
lapshuntanya <lapshun.tatiana@gmail.com>
2024-07-04 20:16:02 +0300

    main

--- PV311-Lesson37-Queue/PV311-Lesson37-Queue.cpp
+++ PV311-Lesson37-Queue/PV311-Lesson37-Queue.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include "MyQueue.h"
+#include <string>
+using std::string;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MyQueue::Queue<string> pv311;
+
+    pv311.push_back("Олег");
+    pv311.push_back("Данило");
+    pv311.push_back("Ярослав");
+    pv311.showInfo();
+
+    cout << "______________________________\n";
+    while (!pv311.isEmpty())
+    {
+        pv311.showInfo();
+        cout << pv311.first() << endl << endl;
+        pv311.pop_front();
+    }
 }
 

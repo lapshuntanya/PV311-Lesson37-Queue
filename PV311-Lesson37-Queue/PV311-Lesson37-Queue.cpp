242f15fab6bd8ca607d93d528464e23172cbc27b
lapshuntanya <lapshun.tatiana@gmail.com>
2024-07-04 21:20:51 +0300

    MyQueue::PriorityQueue<int> a; main

--- PV311-Lesson37-Queue/PV311-Lesson37-Queue.cpp
+++ PV311-Lesson37-Queue/PV311-Lesson37-Queue.cpp
@@ -1,10 +1,11 @@
 #include "MyQueue.h"
+#include "PriorityQueue.h"
 #include <string>
 using std::string;
 
 int main()
 {
-    MyQueue::Queue<string> pv311;
+    /*MyQueue::Queue<string> pv311;
 
     pv311.push_back("Олег");
     pv311.push_back("Данило");
@@ -17,6 +18,23 @@ int main()
         pv311.showInfo();
         cout << pv311.first() << endl << endl;
         pv311.pop_front();
+    }*/
+
+    MyQueue::PriorityQueue<int> a;
+
+    a.push_back(34);
+    a.push_back(64);
+    a.push_back(57);
+    a.push_back(12);
+    a.push_back(92);
+    a.showInfo();
+    cout << "______________________________\n";
+    while (!a.isEmpty())
+    {
+        a.showInfo();
+        cout << a.first() << endl << endl;
+        a.pop_front();
     }
+
 }
 
